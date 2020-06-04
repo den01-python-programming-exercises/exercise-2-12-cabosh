03ecf7232d230263abeea2a9444bedd64a8d88b7
cabosh <callum.bosher@gmail.com>
2020-06-04 23:43:29 +0100

    Attempted commit

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,23 @@
 def main():
     #write your code below this line
 
+    amount = 0
+    total = 0
+
+    while True:
+
+        number = int(input("Enter a number: "))
+
+        if number > 0:
+            amount = amount + 1
+            total = total + number
+        elif number < 0:
+            amount = amount + 1
+            total = total + number
+        elif number == 0:
+            average = total / amount
+            print(f"The average of the numbers is: {average}")
+            break
+
 if __name__ == '__main__':
     main()
